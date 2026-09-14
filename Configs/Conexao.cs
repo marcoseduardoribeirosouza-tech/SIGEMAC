0dd6e42f1348a4b67d5ba0a41e928f384d53beb8
Cyber-Ctrl <neumanngabriel02@gmail.com>
2026-09-14 09:35:18 -0400

    umas alterações ai

--- Configs/Conexao.cs
+++ Configs/Conexao.cs
@@ -1,5 +1,27 @@
-﻿<h3>Conexao</h3>
+﻿using MySql.Data.MySqlClient;
+namespace SIGEMAC.Configs
+{
+    public class Conexao
+    {
 
-@code {
+        private readonly string _connectionString;
+        public Conexao(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("MySqlConnection") ?? "";
+        }
 
+        public MySqlConnection GetConnection()
+        {
+            var conn = new MySqlConnection(_connectionString);
+            conn.Open();
+            return conn;
+        }
+
+        public MySqlCommand CreateCommand(string query, MySqlConnection? conn = null)
+        {
+            conn ??= GetConnection();
+            return new MySqlCommand(query, conn);
+        }
+
+    }
 }
