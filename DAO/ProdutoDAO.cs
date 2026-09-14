af1e95c38e8ea1f3995cb7e8cb7d0cb472807a29
marcoseduardoribeirosouza-tech <marcoseduardoribeirosouza@gmail.com>
2026-09-14 11:55:44 -0400

    Alterações e Produto
    Alterações no Fornecedor
    
    Adicionado ProdutoDAO
    Adicionado Model de Produto

--- DAO/ProdutoDAO.cs
+++ DAO/ProdutoDAO.cs
@@ -3,21 +3,21 @@ using sigemac.Models;
 
 namespace sigemac.DAO
 {
-    public class FornecedorDAO
+    public class ProdutoDAO
     {
         private readonly Conexao _conexao;
-        public FornecedorDAO(Conexao conexao)
+        public ProdutoDAO(Conexao conexao)
         {
             _conexao = conexao;
         }
 
-        public List<Fornecedor> Listar()
+        public List<Produto> Listar()
         {
-            var lista = new List<Fornecedor>();
+            var lista = new List<Produto>();
 
             using var con = _conexao.GetConnection();
 
-            string sql = "SELECT * FROM fornecedor";
+            string sql = "SELECT * FROM produto left join fornecedor on (id_forn_fk = id_forn);";
             using var comando = con.CreateCommand();
             comando.CommandText = sql;
 
@@ -25,12 +25,17 @@ namespace sigemac.DAO
 
             while (leitor.Read())
             {
+                var produto = new Produto();
+                produto.Id = leitor.GetInt32("id_pro");
+                produto.Nome = leitor.GetString("nome_pro");
+                produto.Quantidade = leitor.GetInt32("quantidade_pro");
+                produto.Preco = leitor.GetDecimal("preco_pro");
+                produto.Descricao = leitor.GetString("descricao_pro");
+
                 var fornecedor = new Fornecedor();
                 fornecedor.Id = leitor.GetInt32("id_forn");
                 fornecedor.Nome = leitor.GetString("nome_forn");
-                fornecedor.Cnpj = leitor.GetString("cnpj_forn");
-                fornecedor.Telefone = leitor.GetString("telefone_forn");
-                fornecedor.Email = leitor.GetString("email_forn");
+                produto.Fornecedor = fornecedor;
             }
 
             return lista;
---
+++ Models/Produto.cs
@@ -0,0 +1,13 @@
+﻿namespace sigemac.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public int Quantidade { get; set; } 
+        public decimal Preco { get; set; }
+        public string Descricao { get; set; } = string.Empty;
+        public Fornecedor Fornecedor { get; set; }
+
+    }
+}
